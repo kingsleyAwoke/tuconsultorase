cb5f16cab92dca2443803a8becd523937ba76a10
Kingsley Awoke <kingsley_social@yahoo.com>
2024-06-05 12:27:43 +0100

    Added hero background image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,20 +3,12 @@ import { Link } from 'react-router-dom';
 
 const Hero = () => {
   return (
-    <section className='hero'>
-      <div className="container">
-        <div className="hero-content">
-          <h1>Diseño Paginas Web SEO Acelerar Plantillas Joomla &ndash; Consultoria SEO</h1>
-          <div>
-            <p>Somos expertos en diseño de sitios web, seo, desarrollo de plantillas joomla, acelerar sitios joomla, seguridad web y formación joomla!.</p>
-          </div>
+    <section className='hero container'>
+      <div className="hero-content">
+        <h1>Diseño Paginas Web SEO Acelerar Plantillas Joomla &ndash; Consultoria SEO</h1>
+        <div>
+          <p>Somos expertos en diseño de sitios web, seo, desarrollo de plantillas joomla, acelerar sitios joomla, seguridad web y formación joomla!.</p>
         </div>
-
-        <div className="hero-banner">
-            <figure>
-              <img src='images\jumbotron5.png' alt="Hero Banner" width='1024' height='390' />
-            </figure>
-          </div>
       </div>
     </section>
   )
