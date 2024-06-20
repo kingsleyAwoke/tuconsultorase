cbdc3c23b1a97cbe742432495fa80719e31eff7c
Kingsley Awoke <kingsley_social@yahoo.com>
2024-06-20 09:42:20 +0100

    Publicidad page created

--- src/Pages/PublicidadPago.jsx
+++ src/Pages/PublicidadPago.jsx
@@ -1,11 +1,83 @@
-import React from 'react'
+import { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import BreadCrumb from '../components/BreadCrumb';
 
 const PublicidadPago = () => {
+  //   PAGE TITLE
+  useEffect(() => {
+    document.title = 'Publicidad Pago por Clic - Consultoria SEO';
+  }, []);
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <section className='services'>
+        <BreadCrumb />
+        <div className='publicidad-container contact'>
+
+          <dir className="service-content">
+            <div className='column'>
+              <div className="service-content-head">
+                <span>Publicidad Pago por Clic</span>
+              </div>
+
+              <div className="service-content-txt">
+
+                <p><strong>Publicidad Pago por </strong><font><strong>clic </strong>(PPC) es un modelo de <strong>publicidad </strong>
+                de Internet utilizado en sitios web, en la que los anunciantes pagan s&oacute;lo cuando el cibernauta hace clic en su anuncio.</font>
+                <font>Con los motores de b&uacute;squeda, los anunciantes suelen pujar por frases de palabras clave relevantes para su mercado objetivo.</font>
+                <font>Los sitios de contenido suelen cobrar un precio fijo por clic en lugar de utilizar un sistema de licitaci&oacute;n.</font>
+                <font>El concepto de <strong>pago por clic</strong> gu&iacute;a a los empresarios dedicados en cuanto a la forma de ampliar el negocio de la manera m &aacute;s sofisticada.
+                </font><font>Un hombre convertirse en un hombre de negocios realmente inteligente, siguiendo los consejos y trucos de <strong>PPC</strong>
+                .</font></p>
+
+                <p><font>Los anunciantes disfrutan de la <strong>publicidad </strong>PPC porque es simple, tienen un mayor control sobre su campa&ntilde;a publicitaria, y reciben una mejor retroalimentaci&oacute;n sobre su desempe&ntilde;o. Aunque existen muchos proveedores de PPC, Google AdWords, Yahooo! Search Marketing y Microsoft adCenter son los tres operadores de red m&aacute;s grandes, y los tres operan bajo un modelo basado en la oferta-</font>
+                <font>El programa de Google llamado AdWords ofrece <strong>pago </strong>
+                dirigido por clic en los anuncios a trav&eacute;s de sus propios sitios web en los buscadores y una gran cantidad de sitios asociados. </font><font>Google encabeza la lista con su creciente participaci&oacute;n en la b&uacute;squeda de pago, principalmente de su <strong>pago </strong>
+                por clic, servicio de AdWords.
+                </font></p>
+
+                <p><font>La clave para las campa&ntilde;as de PPC eficaz est &aacute;en su estrategia de palabras clave y la calidad de su an&aacute;lisis a los visitantes.</font>
+                <font>Muchas empresas establecen sus campa&ntilde;as de publicidad PPC sin mucha planificaci&oacute;n. Las c</font>
+                <font>ampa&ntilde;as deben estructurarse de modo que las palabras clave improductivos puedan ser filtrados y las de alto rendimiento mejoradas.</font>
+                <font>Para maximizar el rendimiento de la publicidad PPC, usted debe hacer una oferta sobre las frases de palabras clave seleccionadas, escribir persuasivamente, moviendo el texto del anuncio, y tener una p &aacute;gina bien escrita, as&iacute; obtendr &aacute;clics a trav &eacute;s del anuncio.</font>
+                <font>Si usted sabe c &oacute;mo atraer clics al anuncio y ofrecer una soluci&oacute;n a un problema, usted ser &aacute;recompensado con un buen &iacute;ndice de ventas.</font></p>
+
+                <h2><font>&iquest;Por qu &eacute;elegir TuConsultoraSeo para su campa&ntilde;a de pago por clic?</font></h2>
+
+                <ul>
+                  <li><font>Un plan personalizado es dise&ntilde;ado para cada campa&ntilde;a de PPC, mientras que el desarrollo del plan tiene en cuenta los puntos fuertes de nuestros clientes y las directrices establecidas por los motores de b&uacute;squeda m&aacute;s importantes.</font></li>
+                  <li><font>Informamos regularmente al cliente sobre el progreso de las campa&ntilde;as.</font></li>
+                  <li><font>Nuestro equipo est &aacute;permanentemente actualizado sobre nuevas tendencias de marketing digital y le ayudar&aacute;en sus futuros planes de comercializaci&oacute;n de su negocio.</font></li>
+                  <li><font>na bien orientada<strong> publicidad de pago por clic </strong>
+                  le entregara un excelente retorno de su inversi&oacute;n.</font></li>
+                </ul>
+
+                <p>Dependiendo de sus requerimiento se dise&ntilde;ar&aacute;una campa&ntilde;a eficaz de <strong>publicidad pago por clic, </strong>
+                expliquenos en el siguiente formulario qu&eacute;necesita, nos pondremos en contacto con usted con una propuesta, s&oacute;lo entonces usted cancelar&iacute;a el servicio.</p>
+
+                <p><strong>No olvide indicarnos su sitio o url.</strong></p>
+
+                <iframe allowtransparency="true" frameborder="0" src="https://services.codeeta.com/widget/v3/47769/?responsive=pc&amp;parent=https%3A%2F%2F&amp;embed=script&amp;widget_id=47769" height='528'></iframe>
+                
+                <div className="author-section ">
+                  <h4>Sobre la autora</h4>
+
+                  <p>Author: <Link to="/contact" title='Estela Silva' className='primary-color'>Estela Silva</Link></p>
+
+                  <p>Estela Silva es fundadora y desarrollador principal en Tu Consultora Seo, su función principal es el diseño y desarrollo de sitios web Joomla. Ella también es apasionada del posicionamiento en buscadores. Estela ha construido sitios web basados ​​en CMS Joomla desde el año 2000, así como tiendas en línea, cuando no está escribiendo PHP, Javascript o CSS.</p>
+                </div>
+
+                <ul className='pagination-ul'>
+                  <li className='pagination-li'><Link to='/servicios/soporte-joolmla' className='primary-color'>&lt;Anterior</Link></li>
+
+                  <li className='pagination-li'><Link to='/servicios/optimizacion-para-motores' className='primary-color'>Siguiente&gt;</Link></li>
+                </ul>
+              </div>
+            </div>
+          </dir>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default PublicidadPago
