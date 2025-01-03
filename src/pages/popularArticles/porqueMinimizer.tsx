e4c941330cbd80319f360c15bf9020f126c78b9c
Kingsley Awoke <kingsley_social@yahoo.com>
2025-01-03 13:13:13 +0100

    Updated some pages.

--- src/pages/popularArticles/porqueMinimizer.tsx
+++ src/pages/popularArticles/porqueMinimizer.tsx
@@ -11,16 +11,32 @@ import ContactAuthorSection from "../components/ContactAuthorSection"
 
 const blogSeoData: BlogData[] = [
 	{
-		title: '',
+		title: 'Porque minimizar redirecciones',
 		description: [
-			''
+			'Porque minimizar redirecciones: Hay muchas buenas razones para tener redirecciones, pero hay que tener en cuenta que estropean la experiencia del usuario y causan problemas de velocidad. La utilización de un redireccionamiento obliga al navegador a buscar nuevamente la página web perdiendo un tiempo, a veces, notable.',
+			'<br>Las redirecciones web se consiguen utilizando los códigos de estado 301 y 302. He aquí un ejemplo de las cabeceras HTTP 301 en una respuesta:',
+			'<br>HTTP/1.1 301 Moved Permanently',
+			'<br>Location: http://ejemplo.com/nuevauri',
+			'<br>Content-Type: text/html',
+			'<br>Un redireccionamiento 301 es permanente y una redirección 302 es temporal.',
+			'<br>Tal vez una de las redirecciones más usadas ​​en la web es la redirección 301 permitiendo redirigir todo el sitio de la no-www a la versión www de una página web.',
+			'<br>El navegador automáticamente lleva al usuario a la página especificada en el campo Location. Toda la información necesaria para el redireccionamiento está en el encabezado. Ni un archivo con 301 ni uno con 302 es cacheado en la práctica a menos que tenga otras cabeceras, como Expires o Cache-Control.',
+			'<br>La etiqueta meta refresh y JavaScript son otras formas de dirigir a los usuarios a una URL diferente, pero si usted tiene que hacer un redireccionamiento. Conozca otros tipos de redirecciones.',
+			'<br>¿Cómo afectan las redirecciones la velocidad de la <strong>pagina web</strong>?',
+			'<br>Hacen que sus páginas se cargan más lentamente, ya que se pierde tiempo para ir a un lugar sólo para ser redirigido a otro.',
+			'<br>Google sugiere eliminar redirecciones que no sean absolutamente necesarios. Google sugiere la eliminación de ellas port:',
+			'<ul><li>Nunca enlace a una página que sabes que tiene una redirección en ella. Esto sucede cuando se ha creado manualmente la redirección, pero no se actualizo el vínculo en el código HTML para que apunte a la nueva ubicación.</li><li>No requieren más de una redirección para llegar a cualquiera de sus destinos.</li></ul>',
+			'<h2><em>No olvide que su <strong>página web </strong>no carga solo HTML</em></h2>',
+			'Prácticamente todas las <strong>páginas web </strong>requieren otras cosas para cargar. Usted puede pensar que no vuelve a dirigir en el código HTML, pero ¿qué pasa con los archivos CSS, imágenes o secuencias de comandos externos?',
+			'Por otra parte, asegúrese de que todos los enlaces internos a su <strong>página  web </strong>utilizan sistemáticamente la URL canónica.',
+			'Quiza le interese leer Redireccionamientos engañosos'
 		]
 	}
 ]
 
 const PorqueMinimizer = () => {
 	useEffect(() => {
-		document.title = '';
+		document.title = 'Porque minimizar redirecciones - Consultoria SEO';
 	  }, []);
 	return (
 		<Layouts>
--- src/pages/popularArticles/tutorialPermisos.tsx
+++ src/pages/popularArticles/tutorialPermisos.tsx
@@ -7,20 +7,30 @@ import { Layouts } from "../../components/layouts/layouts"
 import { CustomBreadcrumbs } from "../components/breadcrumbs";
 import { PopularArticles } from "../components/popularArticles"
 import { BlogItem } from "../components/blogitem"
-import ContactAuthorSection from "../components/ContactAuthorSection"
+import ContactAuthorSection from "../components/ContactAuthorSection";
+import Permisos from '../../assets/image/popularArticules/permisos.jpg'
 
 const blogSeoData: BlogData[] = [
 	{
-		title: '',
+		title: 'Tutorial permisos correctos de archivos y carpetas joomla',
 		description: [
-			''
+			'Los sistemas operativos como Unix y Linux utilizan <strong>permisos de acceso</strong>, esto significa que saben cómo hacer frente a las solicitudes de acceso a los<strong> archivos y carpetas</strong>. Hay tres tipos de acceso:',
+			'<ul><li>Ejecución: Se denotan con X, los archivos con accesso de ejecución pueden ser ejecutados como programas por parte del usuario.</li><li>Lectura: Se denotan con r (de read), los arcivos con accesso de lectura se pueden mostrar al usuario.</li><li>Escritura:  Se denotan con W (de write), los archivos con accesso de escritura pueden ser modificados por el usuario.</li></ul>',
+			'<br>Los permisos se dividen en 3:',
+			'<ul><li>Usuario. Es el propietario del archivo</li><li>Grupo. Son otros archivos que están en la misma carpeta o grupo.</li><li>Otros. Todos los demás.</li></ul>',
+			'<br>El servidor web debe ser capaz de leer sus páginas web con el fin de ser capaz de mostrarlas en el navegador. Los siguientes permisos se deben configurar para que su sitio web funcione correctamente:',
+			'<ul><li>Todas las carpetas necesitan ser ejecutables por otros. El valor de los permisos es 755, legibles por usuario, grupo y otros.Se ajusta automáticamente cuando se crea la carpeta.</li><li>Todos los archivos, bien sea imágenes, archivos HTML, PHP, log, ico deben ser leídos por otras personas. El valor del permiso es 644. Se ajustan automáticamente al cargar el archivo.</li><li>Todos los archivos CGI, los que se encuentran en la carpeta cgi-bin, necesitan ser ejecutables por otros. El valor es 755 (legible por usuario, grupo y otros, modificable por el usuario, ejecutable por el usuario, grupo y otros. No se ajusta automáticamente al cargar archivos. Es necesario cambiar los permisos de archivos manualmente.</li></ul>',
+			'<ol><li><strong>permisos correctos de archivos y carpetas joomla</strong>: Ingrese a su cuenta de Cpanel.</li><li><strong>permisos correctos de archivos y carpetas joomla</strong>: Abra el administrador de archivos y navegue hasta el archivo o carpeta que desea cambiar.</li><li><strong>permisos correctos de archivos y carpetas joomla</strong>: Haga clic en el archivo o carpeta.</li><li><strong>permisos correctos de archivos y carpetas joomla</strong>: Haga clic en Cambiar permisos en el menú superior.</li><li> <strong>permisos correctos de archivos y carpetas joomla</strong>: Haga clic en tantas casillas como sea necesario para crear los permisos adecuados. Los números de permisos debajo de las casillas de verificación se actualizarán automáticamente.</li><li><strong>permisos correctos de archivos y carpetas joomla</strong>: Por último haga clic en el botón cambiar permisos cuando esté listo. El nuevo nivel de permisos se guarda y la pantalla se actualiza para mostrar el archivo o carpeta modificado.</li></ol>',
+			`<img src=${Permisos} alt="permisos correctos en archivos y carpetas joomla.jpg" title="permisos correctos en archivos y carpetas joomla.jpg" width="593" height="363">`,
+			'<br>Una parte parte importante de tener un sitio web Joomla <strong>seguro</strong>es establecer los permisos correctos para los archivos de Joomla y carpetas. Le recomendamos que siga estas sugerencias para sus permisos:',
+			'<ul><li>* Establezca los permisos para las carpetas de Joomla a <strong>755</strong></li><li>* Establezca los permisos para los archivos de Joomla a <strong>644</strong></li><li>* Establezca los permisos para el archivo configuration.php a <strong>444</strong></li><li>* Nunca use 777 (acceso completo) permisos!</li><li>¡Si te gusto el artículo por favor comparte!</li></ul>'
 		]
 	}
 ]
 
 const TutorialPermisos = () => {
 	useEffect(() => {
-		document.title = '';
+		document.title = 'Tutorial permisos correctos de archivos y carpetas joomla - Consultoria SEO';
 	  }, []);
 	return (
 		<Layouts>
